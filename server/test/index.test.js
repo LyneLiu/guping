21c17a4e43f518e30e0077599a537cca11885fb2
sincerefly <sincerefly@163.com>
2016-01-05 16:26:03 +0800

    something update

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -6,6 +6,7 @@ var request = require('request');
 // var code_url = 'http://nuff.eastmoney.com/EM_Finance2015TradeInterface/JS.ashx?id=0006812';
 // var sh300_url = 'http://nufm2.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?type=CT&cmd=0003001&&sty=AMIC&st=z&sr=1&p=1&ps=1000&cb=&js=callbacksh300&token=beb0a0047196124721f56b0f0ff5a27c';
 
+/*
 describe("test/index.test.js", function () {
 
   it("getData() should return {status: 0, ...}", function (done) {
@@ -19,8 +20,5 @@ describe("test/index.test.js", function () {
     });
   });
 
-  /*
-  it("add should return {status: 0, ...}", function (done) {
-  });
- */
 });
+*/
